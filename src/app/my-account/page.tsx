63f409bd8aef21ba1e9e7087e09401b65fe2c63c
Diana Cristy Florez Bravo <dianacristyflorezbravo@Dianas-MBP.bbrouter>
2025-03-07 17:55:22 -0500

    [add] chat bot con openai

---
+++ src/app/my-account/page.tsx
@@ -0,0 +1,8 @@
+
+export default async function MyAccountPage() {
+  return (
+    <div>
+      <h2>My Account</h>
+    </div>
+  );
+}
--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,5 @@ export const env = {
     CACHE_TOKEN: process.env.CACHE_TOKEN,
     SHOPIFY_GRAPHQL_ENDPOINT: process.env.SHOPIFY_GRAPHQL_ENDPOINT as string,
     SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN as string,
+    OPENAI_API_KEY: process.env.OPEN_AI_KEY as string,
   }
